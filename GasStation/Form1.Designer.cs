dc7897b34186031adb6831403c998ecda5a9fa75
ParanovaFactory <berkay_krdmn@hotmail.com>
2024-10-03 15:47:58 +0300

    Add project files.

---
+++ GasStation/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace GasStation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSell = new System.Windows.Forms.Button();
+            this.btnBuy = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvCash = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvStck = new System.Windows.Forms.DataGridView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dgvSales = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLiter = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSellLiter = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPlate = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCash)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStck)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSales)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSell
+            // 
+            this.btnSell.Location = new System.Drawing.Point(77, 89);
+            this.btnSell.Name = "btnSell";
+            this.btnSell.Size = new System.Drawing.Size(121, 44);
+            this.btnSell.TabIndex = 0;
+            this.btnSell.Text = "Sell";
+            this.btnSell.UseVisualStyleBackColor = true;
+            this.btnSell.Click += new System.EventHandler(this.btnSell_Click);
+            // 
+            // btnBuy
+            // 
+            this.btnBuy.Location = new System.Drawing.Point(63, 57);
+            this.btnBuy.Name = "btnBuy";
+            this.btnBuy.Size = new System.Drawing.Size(121, 44);
+            this.btnBuy.TabIndex = 1;
+            this.btnBuy.Text = "Buy";
+            this.btnBuy.UseVisualStyleBackColor = true;
+            this.btnBuy.Click += new System.EventHandler(this.btnBuy_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvCash);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(236, 114);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cash Register";
+            // 
+            // dgvCash
+            // 
+            this.dgvCash.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCash.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvCash.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCash.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvCash.Location = new System.Drawing.Point(3, 22);
+            this.dgvCash.Name = "dgvCash";
+            this.dgvCash.Size = new System.Drawing.Size(230, 89);
+            this.dgvCash.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvStck);
+            this.groupBox2.Location = new System.Drawing.Point(12, 161);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(660, 273);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Stock";
+            // 
+            // dgvStck
+            // 
+            this.dgvStck.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvStck.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvStck.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStck.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvStck.Location = new System.Drawing.Point(3, 22);
+            this.dgvStck.Name = "dgvStck";
+            this.dgvStck.Size = new System.Drawing.Size(654, 248);
+            this.dgvStck.TabIndex = 0;
+            this.dgvStck.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvStck_CellClick);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dgvSales);
+            this.groupBox3.Location = new System.Drawing.Point(12, 450);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(660, 279);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Sales";
+            // 
+            // dgvSales
+            // 
+            this.dgvSales.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvSales.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvSales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSales.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvSales.Location = new System.Drawing.Point(3, 22);
+            this.dgvSales.Name = "dgvSales";
+            this.dgvSales.Size = new System.Drawing.Size(654, 254);
+            this.dgvSales.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Liter";
+            // 
+            // txtLiter
+            // 
+            this.txtLiter.Location = new System.Drawing.Point(63, 25);
+            this.txtLiter.Name = "txtLiter";
+            this.txtLiter.Size = new System.Drawing.Size(121, 26);
+            this.txtLiter.TabIndex = 6;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Controls.Add(this.txtLiter);
+            this.groupBox4.Controls.Add(this.btnBuy);
+            this.groupBox4.Location = new System.Drawing.Point(254, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(196, 117);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Gas Buy";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.label5);
+            this.groupBox5.Controls.Add(this.txtSellLiter);
+            this.groupBox5.Controls.Add(this.label4);
+            this.groupBox5.Controls.Add(this.txtPlate);
+            this.groupBox5.Controls.Add(this.btnSell);
+            this.groupBox5.Location = new System.Drawing.Point(456, 12);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(213, 143);
+            this.groupBox5.TabIndex = 10;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Gas Sell";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Liter";
+            // 
+            // txtSellLiter
+            // 
+            this.txtSellLiter.Location = new System.Drawing.Point(77, 57);
+            this.txtSellLiter.Name = "txtSellLiter";
+            this.txtSellLiter.Size = new System.Drawing.Size(121, 26);
+            this.txtSellLiter.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Plate";
+            // 
+            // txtPlate
+            // 
+            this.txtPlate.Location = new System.Drawing.Point(77, 25);
+            this.txtPlate.Name = "txtPlate";
+            this.txtPlate.Size = new System.Drawing.Size(121, 26);
+            this.txtPlate.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 741);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCash)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStck)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSales)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSell;
+        private System.Windows.Forms.Button btnBuy;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvCash;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvStck;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dgvSales;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLiter;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSellLiter;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPlate;
+    }
+}
+
---
+++ GasStation/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GasStation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection(@"Data Source=predator;Initial Catalog=Db_GasStation;Integrated Security=True;TrustServerCertificate=True");
+
+        void listCash()
+        {
+            SqlCommand cmd = new SqlCommand("select * from TblCashReg", conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dgvCash.DataSource = dt;
+        }
+
+        void listStock()
+        {
+            SqlCommand cmd = new SqlCommand("select * from TblGas", conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dgvStck.DataSource = dt;
+        }
+
+        void listSales()
+        {
+            SqlCommand cmd = new SqlCommand("select * from TblOperation", conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dgvSales.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listCash();
+            listSales();
+            listStock();
+        }
+
+        string id, type;
+        decimal price;
+
+        private void btnBuy_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("update TblGas set stock += @p1 where Id = @p2", conn);
+            cmd.Parameters.AddWithValue("@p1", decimal.Parse(txtLiter.Text));
+            cmd.Parameters.AddWithValue("@p2", id);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            listStock();
+
+        }
+
+        private void btnSell_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("insert into TblOperation (carPlate, type, liter, bill) values (@p1, @p2, @p3, @p4)", conn);
+            cmd.Parameters.AddWithValue("@p1", txtPlate.Text);
+            cmd.Parameters.AddWithValue("@p2", type);
+            cmd.Parameters.AddWithValue("@p3", decimal.Parse(txtSellLiter.Text));
+            cmd.Parameters.AddWithValue("@p4", decimal.Parse(txtSellLiter.Text) * price);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            conn.Open();
+            SqlCommand command = new SqlCommand("update TblCashReg set CashRegister += @p5", conn);
+            command.Parameters.AddWithValue("@p5", decimal.Parse(txtSellLiter.Text) * price);
+            command.ExecuteNonQuery();
+            conn.Close();
+
+            conn.Open();
+            SqlCommand sqlCommand = new SqlCommand("update TblGas set stock -= @p1 where Id = @p2", conn);
+            sqlCommand.Parameters.AddWithValue("@p1", decimal.Parse(txtSellLiter.Text));
+            sqlCommand.Parameters.AddWithValue("@p2", id);
+            sqlCommand.ExecuteNonQuery();
+            conn.Close();
+
+            listCash();
+            listSales();
+            listStock();
+        }
+
+        private void dgvStck_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            id = dgvStck.Rows[e.RowIndex].Cells[0].Value.ToString();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("select gasType, salePrice from TblGas where Id = @p1", conn);
+            cmd.Parameters.AddWithValue("@p1", id);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                type = dr.GetString(0);
+                price = dr.GetDecimal(1);
+            }
+            conn.Close();
+        }
+    }
+}
